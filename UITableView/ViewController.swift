7e6c011ee3a4c1f2c090eec230ec9414927754df
Abdulkarim <Abdulkarim.Alansari@outlook.com>
2021-09-11 00:17:45 +0300

    Initial Commit

---
+++ UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Abdulkarim Alansari on 04/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
