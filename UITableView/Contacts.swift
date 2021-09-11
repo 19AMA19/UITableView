4e5638f3fd20fa3b8a583d01ad8e4052600e1d1c
Abdulkarim <Abdulkarim.Alansari@outlook.com>
2021-09-11 16:08:13 +0300

    Last Update

---
+++ UITableView/Contacts.swift
@@ -0,0 +1,29 @@
+//
+//  Contacts.swift
+//  UITableView
+//
+//  Created by Abdulkarim Alansari on 04/02/1443 AH.
+//
+
+import UIKit
+
+class Contacts: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
